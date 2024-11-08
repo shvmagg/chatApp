d1a3ad1759700b3da713b16a64eabee190686501
shvmagg <aggshivam0407@gmail.com>
2024-11-08 20:44:42 +0530

    request models converted into django models

--- websockets/request_models.py
+++ websockets/request_models.py
@@ -1,30 +1,30 @@
 from django.db import models
-class Request:
-    type:str
-    text:str
+class Request(models.Model):
+    type:models.CharField
+    text:models.CharField
     def __init__(self,**kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-class AuthRequest: #{"senderId":2,"msgType":"auth"}
-    senderId:int
-    msgType:str
+class AuthRequest(models.Model): #{"senderId":2,"msgType":"auth"}
+    senderId:models.BigIntegerField
+    msgType:models.CharField
     def __init__(self, senderId, msgType):
         self.senderId = senderId
         self.msgType = msgType
     
-class LogOutRequest: #{"senderId":2,"msgType":"auth"}
-    senderId:int
-    msgType:str
+class LogOutRequest(models.Model): #{"senderId":2,"msgType":"auth"}
+    senderId:models.BigIntegerField
+    msgType:models.CharField
     def __init__(self, senderId):
         self.senderId=senderId
         self.msgType='logOut'
 
-class SendMessageRequest:
-    recieverId:int
+class SendMessageRequest(models.Model):
+    recieverId:models.CharField
     #senderId:int
-    msg:str
-    msgType:str
+    msg:models.CharField
+    msgType:models.CharField
     def __init__(self,**kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
